809091fda8ceb27e5adb4c700e9d803a0eb72c46
Vasiliy Pupkin <vpupkin@company.com>
2020-07-01 18:42:41 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-/************** animsition  *******************/
-$(".animsition").animsition({
-  inClass: 'fade-in-up-sm',
-  outClass: 'fade-out-up-sm',
-  inDuration: 1100,
-  outDuration: 800,
-  linkElement: '.animsition-link',
-  // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-  loading: true,
-  loadingParentElement: 'body', //animsition wrapper element
-  loadingClass: 'animsition-loading',
-  loadingInner: '', // e.g '<img src="loading.svg" />'
-  timeout: false,
-  timeoutCountdown: 5000,
-  onLoadEvent: true,
-  browser: ['animation-duration', '-webkit-animation-duration'],
-  // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-  // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-  overlay: false,
-  overlayClass: 'animsition-overlay-slie',
-  overlayParentElement: 'body',
-  transition: function (url) {
-    window.location.href = url;
-  }
-});
-
 /**********   burger (выпадающие меню-desktop) ****************/
 $(document).ready(function () {
   var _link = $(".first-level").children("li");
