5a1f9c2c41a5867e3464e362692196635c752165
Vasiliy Pupkin <vpupkin@company.com>
2020-07-08 17:21:02 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -118,3 +118,29 @@ $(".input input").blur(function () {
     $(this).next("span").removeClass("active");
   }
 });
+
+/*************    animsition   ******************/
+$(".animsition").animsition({
+  inClass: 'fade-in-up-sm',
+  outClass: 'fade-out-up-sm',
+  inDuration: 1100,
+  outDuration: 800,
+  linkElement: '.animsition-link',
+  // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+  loading: true,
+  loadingParentElement: 'body', //animsition wrapper element
+  loadingClass: 'animsition-loading',
+  loadingInner: '', // e.g '<img src="loading.svg" />'
+  timeout: false,
+  timeoutCountdown: 5000,
+  onLoadEvent: true,
+  browser: ['animation-duration', '-webkit-animation-duration'],
+  // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+  // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+  overlay: false,
+  overlayClass: 'animsition-overlay-slide',
+  overlayParentElement: 'body',
+  transition: function (url) {
+    window.location.href = url;
+  }
+});
